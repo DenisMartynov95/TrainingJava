7ab1a7b291b59a3db0fe4724953d1567887ed71e
Denis Martynov <idenismartynov@yandex.ru>
2024-11-24 01:23:05 +0300

    Балуюсь задачками

--- src/main/java/BigTask/FoodPack.java
+++ src/main/java/BigTask/FoodPack.java
@@ -7,8 +7,12 @@ import java.util.Scanner;
 public class FoodPack {
 
     private String[] generalFoodPack = new String[] {"№1 Рыба", "№2 Мясо", "№3 Грибы"};
+    HashMap<Integer, String> subFoodPack = new HashMap<>();
+
     private Scanner scanner = new Scanner(System.in);
-    private int chosenNumber;
+    private int chosenNumberGeneralFood;
+    private int chosenNumberSubFood;
+
 
     public String[] getGeneralFoodPack() {
         return generalFoodPack;
@@ -23,10 +27,10 @@ public class FoodPack {
         this.scanner = scanner;
     }
     public int getChosenNumber() {
-        return chosenNumber;
+        return chosenNumberGeneralFood;
     }
     public void setChosenNumber(int chosenNumber) {
-        this.chosenNumber = chosenNumber;
+        this.chosenNumberGeneralFood = chosenNumber;
     }
 
 
@@ -38,7 +42,6 @@ public class FoodPack {
     }
 
     public void printSubFoodPack() {
-        HashMap<Integer, String> subFoodPack = new HashMap<>();
         subFoodPack.put(1, "Путасу");
         subFoodPack.put(2, "Щука");
         subFoodPack.put(3, "Оленина");
@@ -54,21 +57,67 @@ public class FoodPack {
         System.out.println("Введите число от 1 до 3 для выбора: ");
         int number = scanner.nextInt();
         if (number == 1) {
-            chosenNumber = number;
+            chosenNumberGeneralFood = number;
             System.out.println(generalFoodPack[0]);
         } else if (number == 2) {
-            chosenNumber = number;
+            chosenNumberGeneralFood = number;
             System.out.println(generalFoodPack[1]);
         } else if (number == 3) {
-            chosenNumber = number;
+            chosenNumberGeneralFood = number;
             System.out.println(generalFoodPack[2]);
         } else {
             System.out.println("Число было введенно некорректно - введите новое!");
-            System.out.println(" ");
         }
+        System.out.println(" ");
     }
 
     public void choseSubFood() {
+        System.out.println("Выберите подпродукт из двух вариантов: ");
+
+        int number = chosenNumberGeneralFood; // Используем уже выбранный общий продукт
+        if (number == 1) {
+            System.out.println("1 - " + subFoodPack.get(1));
+            System.out.println("2 - " + subFoodPack.get(2));
+        } else if (number == 2) {
+            System.out.println("1 - " + subFoodPack.get(3));
+            System.out.println("2 - " + subFoodPack.get(4));
+        } else if (number == 3) {
+            System.out.println("1 - " + subFoodPack.get(5));
+            System.out.println("2 - " + subFoodPack.get(6));
+        } else {
+            System.out.println("Ошибка! Общий продукт не выбран.");
+            return;
+        }
+
+        System.out.println("Введите 1 или 2: ");
+        int choseSubFood = scanner.nextInt();
+
+        if (number == 1) {
+            if (choseSubFood == 1) {
+                System.out.println("Вы выбрали: " + subFoodPack.get(1));
+            } else if (choseSubFood == 2) {
+                System.out.println("Вы выбрали: " + subFoodPack.get(2));
+            } else {
+                System.out.println("Некорректный выбор!");
+            }
+        } else if (number == 2) {
+            if (choseSubFood == 1) {
+                System.out.println("Вы выбрали: " + subFoodPack.get(3));
+            } else if (choseSubFood == 2) {
+                System.out.println("Вы выбрали: " + subFoodPack.get(4));
+            } else {
+                System.out.println("Некорректный выбор!");
+            }
+        } else if (number == 3) {
+            if (choseSubFood == 1) {
+                System.out.println("Вы выбрали: " + subFoodPack.get(5));
+            } else if (choseSubFood == 2) {
+                System.out.println("Вы выбрали: " + subFoodPack.get(6));
+            } else {
+                System.out.println("Некорректный выбор!");
+            }
+        }
+    }
 
 
     }
