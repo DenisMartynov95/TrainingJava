e6d23213341c334bc4fe0ced43102180b71de31e
Denis Martynov <idenismartynov@yandex.ru>
2025-09-23 19:00:07 +0300

    Решаю задачки

--- src/main/java/Training1/MapTraining2.java
+++ src/main/java/Training1/MapTraining2.java
@@ -16,7 +16,7 @@ public class MapTraining2 {
      */
 
     final String textLine = "apple orange apple banana orange apple";
-    LinkedHashMap<Integer,String> setList = new LinkedHashMap<>(); // В нем буду хранить только уникальные значения
+    LinkedHashMap<Integer,String> setList = new LinkedHashMap<>(); // В нем буду хранить только уникальные значения. так как нужны индексы - создал LinkedHashMap а не мапу
     String[] textContainer; // Пустой контейнер, чтобы я переложил в следующем методе разделенные значения
 
     public void splitTextLine() { // Код для разделения строки по пробелам в массив
@@ -39,16 +39,14 @@ public class MapTraining2 {
     }
 
     public void countRepeats() {
-        Map<Integer, String> listOfRepeats = new HashMap<>();
-        int numberCount = 0;
+        Map<String, Integer> listOfRepeats = new HashMap<>();
 
-        for (int i =0; i < textContainer.length; i++) {
-            if (setList.contains(textContainer[i]) && setList.con) {
-                numberCount = numberCount + 1;
-                listOfRepeats.put(numberCount,textContainer[i]);
-            }
+        for (int i = 0; i < textContainer.length; i++) {
+            int oldValue = listOfRepeats.getOrDefault(textContainer[i], 0);
+            listOfRepeats.put(textContainer[i], oldValue + 1); // сразу увеличиваем
         }
-        System.out.println(listOfRepeats);
+
+        System.out.println(listOfRepeats.entrySet());
     }
 
 }
