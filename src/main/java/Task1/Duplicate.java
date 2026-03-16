03c03ba4c681ec162063b80cecf32244908127d8
Denis Martynov <idenismartynov@yandex.ru>
2026-03-16 16:16:56 +0300

    Решаю задачки

--- src/main/java/Task1/Duplicate.java
+++ src/main/java/Task1/Duplicate.java
@@ -1,8 +1,6 @@
 package Task1;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 /*
 Поиск дубликатов в массиве
@@ -13,11 +11,12 @@ import java.util.Set;
 Подсказка: Используй HashSet для уникальных элементов и второй сет для дубликатов. Или можно посчитать частоту через HashMap.
  */
 public class Duplicate {
-    public int[] arrNumbers = new int[]{4,3,2,7,8,2,3,1,5,4,0,10};
+    public int[] arrNumbers = new int[]{4,3,2,7,8,2,3,1,5,4,0,10,4,5,3,10};
 
     public void checkDuplicate() {
         Set<Integer> numbers = new HashSet<>();
         Set<Integer> duplicates = new HashSet<>();
+
 
         for (int i =0; i < arrNumbers.length;i++) {
             if (!numbers.add(arrNumbers[i])) {
@@ -25,6 +24,22 @@ public class Duplicate {
             }
         }
         System.out.println(duplicates);
+
+
+
+        Map<Integer, Integer> frequency = new HashMap<>(); // Для подсчета дубликатов
+        for (int num : arrNumbers) {
+            frequency.put(num, frequency.getOrDefault(num, 0) + 1);
+        }
+        System.out.println("Частота всех чисел: " + frequency);
+
+        // Выводим только те, у которых частота > 1
+        System.out.println("Числа с частотой > 1:");
+        for (Map.Entry<Integer, Integer> entry : frequency.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.println(entry.getKey() + " -> " + entry.getValue());
+            }
+        }
 
     }
 
